18818bb2f830fbc49f0e2a65a2b458e2deb08419
Marcos Ramón <marcosramon.alu@iespacomolla.es>
2024-02-07 17:29:41 +0100

    footer,menuhamburguesa y arreglo logo

--- src/components/estructura/MenuHamburguesa.jsx
+++ src/components/estructura/MenuHamburguesa.jsx
@@ -14,15 +14,28 @@ const MenuHamburguesa = () => {
           onClick={() => {
             setEstadoMenu(true);
           }}
-          className="icono-menu-hamburguesa"
+          height="4em"
+          width="4em"
+          enableBackground="new 0 0 32 32"
+          id="Glyph"
+          version="1.1"
+          viewBox="0 0 32 32"
+          xmlSpace="preserve"
           xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="100"
-          height="100"
-          viewBox="0 0 50 50"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
         >
-          <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
+          <path
+            d="M26,16c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,14,26,14.896,26,16z"
+            id="XMLID_314_"
+          />
+          <path
+            d="M26,8c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,6,26,6.896,26,8z"
+            id="XMLID_315_"
+          />
+          <path
+            d="M26,24c0,1.104-0.896,2-2,2H8c-1.104,0-2-0.896-2-2s0.896-2,2-2h16C25.104,22,26,22.896,26,24z"
+            id="XMLID_316_"
+          />
         </svg>
       )}
       {estadoMenu && (
