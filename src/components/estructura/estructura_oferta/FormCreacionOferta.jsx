e0506ff63b8afafc9d368b4007e3b1f9eb89fff0
Marcos Ramón <marcosramon.alu@iespacomolla.es>
2024-02-21 20:24:02 +0100

    css formulario creacion

--- src/components/estructura/estructura_oferta/FormCreacionOferta.jsx
+++ src/components/estructura/estructura_oferta/FormCreacionOferta.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Select from "react-select";
+import "./FormCreacionOferta.scss";
 
 const FormCreacionOferta = () => {
   const options = [
@@ -10,35 +11,46 @@ const FormCreacionOferta = () => {
     { value: "MusicaAudio", label: "MusicaAudio" },
     { value: "ProgamacionTecnologia", label: "Progamación y Tecnologia" },
   ];
+
   return (
     <div className="form-creacion-oferta">
-      <div className="imagen-oferta">
-        <label htmlFor="">Imagen de la Oferta</label>
-        <input type="file" placeholder="Example Imagen" />
-        <img src="#" alt="" />
+      <div className="left-column">
+        <div className="nombre-oferta">
+          <label className="label" htmlFor="">
+            Nombre del Oferta:
+          </label>
+          <input type="text" placeholder="Example Oferta" />
+        </div>
+        <div className="descripcion-oferta">
+          <label className="label" htmlFor="">
+            Descripción de la Oferta:
+          </label>
+          <textarea type="text" placeholder="Example Descripción" />
+        </div>
+        <div className="precio-oferta">
+          <label className="label" htmlFor="">
+            Precio de la Oferta:
+          </label>
+          <input type="number" min={1} />
+        </div>
       </div>
-      <div className="nombre-oferta">
-        <label htmlFor="">Nombre del Oferta</label>
-        <input type="text" placeholder="Example Oferta" />
+      <div className="right-column">
+        <div className="categoria-oferta">
+          <label htmlFor="">Selecciona una Categoría:</label>
+          <Select
+            onChange={(e) => {
+              console.log(e.value);
+            }}
+            options={options}
+          />
+        </div>
+        <div className="imagen-oferta">
+          <label htmlFor="">Imagen de la Oferta:</label>
+          <input type="file" placeholder="Example Imagen" />
+          <img src="#" alt="" />
+        </div>
+        <button className="boton-oferta">Crear Oferta</button>
       </div>
-      <div className="descripcion-oferta">
-        <label htmlFor="">Nombre del Oferta</label>
-        <input type="text" placeholder="Example Descripción" />
-      </div>
-      <div className="cateogria-oferta">
-        <label htmlFor="">Selecciona una Categoría</label>
-        <Select
-          onChange={(e) => {
-            console.log(e.value);
-          }}
-          options={options}
-        />
-      </div>
-      <div className="precio-oferta">
-        <label htmlFor="">Precio de la Oferta</label>
-        <input type="number" min={1} />
-      </div>
-      <button>Crear Oferta</button>
     </div>
   );
 };
