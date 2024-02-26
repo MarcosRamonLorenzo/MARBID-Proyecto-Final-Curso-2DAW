5f83f3fd4694df810a9e2e672cfc9224b8edf3fd
xXxXxDavidxXxXx <davidcasanyez.alu@iespacomolla.es>
2024-02-26 21:48:27 +0100

    Falta por arreglar el click del perfil

--- src/components/estructura/estructura_panel_control/MenuLateral.jsx
+++ src/components/estructura/estructura_panel_control/MenuLateral.jsx
@@ -20,77 +20,77 @@ const MenuLateral = () => {
           />
         </Link>
 
-        <div
-          className={`ofertas-creadas contenedor-enlace ${
-            enlaceClicado === "ofertas-creadas" ? "clicado" : ""
-          }`}
+        <Link
+          to={"./OfertasCreadas"}
+          onClick={() => handleClick("ofertas-creadas")}
         >
-          <img
-            className="icono-lista"
-            src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-creadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1jcmVhZGFzLnN2ZyIsImlhdCI6MTcwODc3NDc0MywiZXhwIjoxNzQwMzEwNzQzfQ.F07bzbysDmwLnU74Nj_wfUAtZ3vLBq4xyhQ7pwndzJI&t=2024-02-24T11%3A39%3A03.829Z"
-            alt=""
-          />
-          <Link
-            to={"./OfertasCreadas"}
-            onClick={() => handleClick("ofertas-creadas")}
+          <div
+            className={`ofertas-creadas contenedor-enlace ${
+              enlaceClicado === "ofertas-creadas" ? "clicado" : ""
+            }`}
           >
-            Ofertas Creadas
-          </Link>
-        </div>
+            <img
+              className="icono-lista"
+              src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-creadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1jcmVhZGFzLnN2ZyIsImlhdCI6MTcwODc3NDc0MywiZXhwIjoxNzQwMzEwNzQzfQ.F07bzbysDmwLnU74Nj_wfUAtZ3vLBq4xyhQ7pwndzJI&t=2024-02-24T11%3A39%3A03.829Z"
+              alt=""
+            />
+            <a>Ofertas Creadas</a>
+          </div>
+        </Link>
 
-        <div
-          className={`ofertas-aplicadas contenedor-enlace ${
-            enlaceClicado === "ofertas-aplicadas" ? "clicado" : ""
-          }`}
+        <Link
+          to={"./OfertasAplicadas"}
+          onClick={() => handleClick("ofertas-aplicadas")}
         >
-          <img
-            className="icono-lista"
-            src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-aplicadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1hcGxpY2FkYXMuc3ZnIiwiaWF0IjoxNzA4Nzc0ODA3LCJleHAiOjE3NDAzMTA4MDd9.AtfFj7PNLgCyKJChuIzbj940zsJOoVSMgPeRBi1XxWs&t=2024-02-24T11%3A40%3A07.845Z"
-            alt=""
-          />
-          <Link
-            to={"./OfertasAplicadas"}
-            onClick={() => handleClick("ofertas-aplicadas")}
+          <div
+            className={`ofertas-aplicadas contenedor-enlace ${
+              enlaceClicado === "ofertas-aplicadas" ? "clicado" : ""
+            }`}
           >
-            Ofertas Aplicadas
-          </Link>
-        </div>
+            <img
+              className="icono-lista"
+              src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-aplicadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1hcGxpY2FkYXMuc3ZnIiwiaWF0IjoxNzA4Nzc0ODA3LCJleHAiOjE3NDAzMTA4MDd9.AtfFj7PNLgCyKJChuIzbj940zsJOoVSMgPeRBi1XxWs&t=2024-02-24T11%3A40%3A07.845Z"
+              alt=""
+            />
+            <a>Ofertas Aplicadas</a>
+          </div>
+        </Link>
 
-        <div
-          className={`ofertas-gustadas contenedor-enlace ${
-            enlaceClicado === "ofertas-gustadas" ? "clicado" : ""
-          }`}
+        <Link
+          to={"./Favoritos"}
+          onClick={() => handleClick("ofertas-gustadas")}
         >
-          <img
-            className="icono-lista"
-            src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/corazon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvY29yYXpvbi5zdmciLCJpYXQiOjE3MDg3NzQ3NjgsImV4cCI6MTc0MDMxMDc2OH0.HMA2uLI8O3KafDgszcLN-wRGIucz_w-TEQD2Xe7Mo8Q&t=2024-02-24T11%3A39%3A28.206Z"
-            alt=""
-          />
-          <Link
-            to={"./Favoritos"}
-            onClick={() => handleClick("ofertas-gustadas")}
+          <div
+            className={`ofertas-gustadas contenedor-enlace ${
+              enlaceClicado === "ofertas-gustadas" ? "clicado" : ""
+            }`}
           >
-            Ofertas Gustadas
-          </Link>
-        </div>
+            <img
+              className="icono-lista"
+              src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/corazon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvY29yYXpvbi5zdmciLCJpYXQiOjE3MDg3NzQ3NjgsImV4cCI6MTc0MDMxMDc2OH0.HMA2uLI8O3KafDgszcLN-wRGIucz_w-TEQD2Xe7Mo8Q&t=2024-02-24T11%3A39%3A28.206Z"
+              alt=""
+            />
+            <a>Ofertas Gustadas</a>
+          </div>
+        </Link>
 
-        <div
-          className={`crear-oferta contenedor-enlace ${
-            enlaceClicado === "crear-oferta" ? "clicado" : ""
-          }`}
+        <Link
+          to={"./CreacionProducto"}
+          onClick={() => handleClick("crear-oferta")}
         >
-          <img
-            className="icono-lista"
-            src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/agregarOferta.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvYWdyZWdhck9mZXJ0YS5zdmciLCJpYXQiOjE3MDg3NzQ5NjcsImV4cCI6MTc0MDMxMDk2N30.AjKdDurg7sW8i1AcAnKIWdXlFtyIt4XdHVMcU2lUtI8&t=2024-02-24T11%3A42%3A47.576Z"
-            alt=""
-          />
-          <Link
-            to={"./CreacionProducto"}
-            onClick={() => handleClick("crear-oferta")}
+          <div
+            className={`crear-oferta contenedor-enlace ${
+              enlaceClicado === "crear-oferta" ? "clicado" : ""
+            }`}
           >
-            Crear Oferta
-          </Link>
-        </div>
+            <img
+              className="icono-lista"
+              src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/agregarOferta.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvYWdyZWdhck9mZXJ0YS5zdmciLCJpYXQiOjE3MDg3NzQ5NjcsImV4cCI6MTc0MDMxMDk2N30.AjKdDurg7sW8i1AcAnKIWdXlFtyIt4XdHVMcU2lUtI8&t=2024-02-24T11%3A42%3A47.576Z"
+              alt=""
+            />
+            <a>Crear Oferta</a>
+          </div>
+        </Link>
       </div>
 
       <div
