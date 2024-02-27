f1648a958786be3a71a15f686fff05493ebb9a08
xXxXxDavidxXxXx <davidcasanyez.alu@iespacomolla.es>
2024-02-27 18:45:14 +0100

    Arreglo menú lateral en panel de control

--- src/components/estructura/estructura_panel_control/MenuLateral.jsx
+++ src/components/estructura/estructura_panel_control/MenuLateral.jsx
@@ -34,7 +34,7 @@ const MenuLateral = () => {
               src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-creadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1jcmVhZGFzLnN2ZyIsImlhdCI6MTcwODc3NDc0MywiZXhwIjoxNzQwMzEwNzQzfQ.F07bzbysDmwLnU74Nj_wfUAtZ3vLBq4xyhQ7pwndzJI&t=2024-02-24T11%3A39%3A03.829Z"
               alt=""
             />
-            <a>Ofertas Creadas</a>
+            <p>Ofertas Creadas</p>
           </div>
         </Link>
 
@@ -52,7 +52,7 @@ const MenuLateral = () => {
               src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/ofertas-aplicadas.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvb2ZlcnRhcy1hcGxpY2FkYXMuc3ZnIiwiaWF0IjoxNzA4Nzc0ODA3LCJleHAiOjE3NDAzMTA4MDd9.AtfFj7PNLgCyKJChuIzbj940zsJOoVSMgPeRBi1XxWs&t=2024-02-24T11%3A40%3A07.845Z"
               alt=""
             />
-            <a>Ofertas Aplicadas</a>
+            <p>Ofertas Aplicadas</p>
           </div>
         </Link>
 
@@ -70,7 +70,7 @@ const MenuLateral = () => {
               src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/corazon.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvY29yYXpvbi5zdmciLCJpYXQiOjE3MDg3NzQ3NjgsImV4cCI6MTc0MDMxMDc2OH0.HMA2uLI8O3KafDgszcLN-wRGIucz_w-TEQD2Xe7Mo8Q&t=2024-02-24T11%3A39%3A28.206Z"
               alt=""
             />
-            <a>Ofertas Gustadas</a>
+            <p>Ofertas Gustadas</p>
           </div>
         </Link>
 
@@ -88,23 +88,24 @@ const MenuLateral = () => {
               src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/agregarOferta.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvYWdyZWdhck9mZXJ0YS5zdmciLCJpYXQiOjE3MDg3NzQ5NjcsImV4cCI6MTc0MDMxMDk2N30.AjKdDurg7sW8i1AcAnKIWdXlFtyIt4XdHVMcU2lUtI8&t=2024-02-24T11%3A42%3A47.576Z"
               alt=""
             />
-            <a>Crear Oferta</a>
+            <p>Crear Oferta</p>
           </div>
         </Link>
       </div>
-
-      <div
-        className={`perfil contenedor-enlace ${
-          enlaceClicado === "perfil" ? "clicado" : ""
-        }`}
-      >
-        <img
-          className="icono-lista"
-          src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/perfil.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvcGVyZmlsLnN2ZyIsImlhdCI6MTcwODc3NTQ3NywiZXhwIjoxNzQwMzExNDc3fQ.3z2qcCLHIlQcQUeB36RQvTbnwwgO4P-IWy3Nu-s5fCA&t=2024-02-24T11%3A51%3A17.921Z"
-          alt=""
-        />
+      <div className="menu-lateral-perfil">
         <Link to={"/"} onClick={() => handleClick("perfil")}>
-          Perfil
+          <div
+            className={`perfil contenedor-enlace ${
+              enlaceClicado === "perfil" ? "clicado" : ""
+            }`}
+          >
+            <img
+              className="icono-lista"
+              src="https://hpagogvjlwigezymiejy.supabase.co/storage/v1/object/sign/IconosSVG/perfil.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJJY29ub3NTVkcvcGVyZmlsLnN2ZyIsImlhdCI6MTcwODc3NTQ3NywiZXhwIjoxNzQwMzExNDc3fQ.3z2qcCLHIlQcQUeB36RQvTbnwwgO4P-IWy3Nu-s5fCA&t=2024-02-24T11%3A51%3A17.921Z"
+              alt=""
+            />
+            <p>Perfil</p>
+          </div>
         </Link>
       </div>
     </div>
