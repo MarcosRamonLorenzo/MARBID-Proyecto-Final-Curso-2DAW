639d24a95595460071f3eec0811e5e6abd30eceb
xXxXxDavidxXxXx <davidcasanyez.alu@iespacomolla.es>
2024-02-28 12:36:43 +0100

    Borrar anuncio

--- src/pages/PanelDeControl.jsx
+++ src/pages/PanelDeControl.jsx
@@ -4,16 +4,22 @@ import "./PanelDeControl.scss";
 import useDatosUsuarios from "../hooks/useDatosUsuarios.js";
 import AlertNoHaySesion from "../components/estructura/alerts/AlertNoHaySesion.jsx";
 import MenuLateral from "../components/estructura/estructura_panel_control/MenuLateral.jsx";
+import useDatosAnuncios from "../hooks/useDatosAnuncio.js";
+import Loading from "../components/estructura/Loading.jsx";
 
 const PanelDeControl = () => {
   const { sesionIniciada } = useDatosUsuarios();
+  const { cargandoAnuncio } = useDatosAnuncios();
   return (
     <Fragment>
       {sesionIniciada ? (
-        <div className="panel-control">
-          <MenuLateral />
-          <Outlet />
-        </div>
+        <>
+          {cargandoAnuncio && <Loading />}
+          <div className="panel-control">
+            <MenuLateral />
+            <Outlet />
+          </div>
+        </>
       ) : (
         <AlertNoHaySesion />
       )}
